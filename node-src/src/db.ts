21bc89aa945c3c614c92f4c540eb18c2468ac6d7
Adeeba Begum <adeeba.begum@slu.edu>
2025-03-02 11:58:56 +0000

    Adeeba-is-5600-week-07-lab

--- node-src/src/db.ts
+++ node-src/src/db.ts
@@ -13,7 +13,7 @@ mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://root:example@db:27017/?authSource=admin',
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true  
   }
 );
 
--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // TODO: Replace this with your own Server URL for your codespace
-export const BASE_URL = "https://glowing-space-memory-q7p4jvr9g4p424rj7-3080.app.github.dev";
+export const BASE_URL = "https://fantastic-computing-machine-4jvv674rx6q73q64q-3080.app.github.dev";
